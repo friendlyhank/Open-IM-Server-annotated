baf63f148c0628710f57c197a52f5091970387a6
hank <hanguang8888@outlook.com>
2023-03-19 23:14:29 +0800

    rpc

--- internal/msg_gateway/gate/relay_rpc_server.go
+++ internal/msg_gateway/gate/relay_rpc_server.go
@@ -4,13 +4,15 @@ import "Open_IM/pkg/common/config"
 
 // rpc服务相关逻辑
 type RPCServer struct {
-	rpcPort  int      // rpc端口
-	etcdAddr []string // etcd地址
+	rpcPort    int // rpc端口
+	etcdSchema string
+	etcdAddr   []string // etcd地址
 }
 
 // rpc服务初始化
 func (r *RPCServer) onInit(rpcPort int) {
 	r.rpcPort = rpcPort
+	r.etcdSchema = config.Config.Etcd.EtcdSchema
 	r.etcdAddr = config.Config.Etcd.EtcdAddr
 }
 
