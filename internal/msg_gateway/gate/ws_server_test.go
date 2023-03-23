1245d8a0191134cf8c251192974457f81a9fa661
李汉光 <lihanguang@xinyu668.com>
2023-03-23 21:41:29 +0800

    mode

--- internal/msg_gateway/gate/ws_server_test.go
+++ internal/msg_gateway/gate/ws_server_test.go
@@ -1,19 +1,14 @@
 package gate
 
 import (
-	"Open_IM/pkg/common/constant"
-	open_im_sdk "Open_IM/pkg/proto/sdk_ws"
-	"bytes"
-	"encoding/gob"
 	"fmt"
-	"github.com/golang/protobuf/proto"
 	"github.com/gorilla/websocket"
 	"log"
 	"testing"
 )
 
 func Test_WServerSendMsg(t *testing.T) {
-	url := "ws://0.0.0.0:10001?token=123&sendID=383&platformID=1"
+	url := "ws://0.0.0.0:10001/?sendID=2781471455&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVSUQiOiIyNzgxNDcxNDU1IiwiUGxhdGZvcm0iOiJXZWIiLCJleHAiOjE2ODczNTE2MjQsIm5iZiI6MTY3OTU3NTMyNCwiaWF0IjoxNjc5NTc1NjI0fQ.LcUe19dMFIN3CDK_OB_DI3UKCRgpkagfopki8AypP7g&platformID=5&operationID=1679578304591406600"
 	c, res, err := websocket.DefaultDialer.Dial(url, nil)
 	if err != nil {
 		log.Fatal("连接失败:", err)
@@ -22,39 +17,39 @@ func Test_WServerSendMsg(t *testing.T) {
 	defer c.Close()
 	done := make(chan struct{})
 
-	// 发送消息结构体
-	msgData := &open_im_sdk.MsgData{
-		SendID: "1",
-		SessionType:constant.SingleChatType,
-	}
-
-	d, _ := proto.Marshal(msgData)
-
-	// im请求结构体
-	m := &Req{
-		ReqIdentifier: 1003,
-		Token:         "123",
-		SendID:        "383",
-		OperationID:   "1",
-		MsgIncr:       "1",
-		Data:          d,
-	}
-
-	var b bytes.Buffer
-	enc := gob.NewEncoder(&b)
-	enc.Encode(m)
-
-	err = c.WriteMessage(websocket.BinaryMessage, b.Bytes())
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	for {
-		_, message, err := c.ReadMessage()
-		if err != nil {
-			log.Fatal(err)
-			break
-		}
-		log.Printf("收到消息: %s", message)
-	}
+	// // 发送消息结构体
+	// msgData := &open_im_sdk.MsgData{
+	// 	SendID: "1",
+	// 	SessionType:constant.SingleChatType,
+	// }
+	//
+	// d, _ := proto.Marshal(msgData)
+	//
+	// // im请求结构体
+	// m := &Req{
+	// 	ReqIdentifier: 1003,
+	// 	Token:         "123",
+	// 	SendID:        "383",
+	// 	OperationID:   "1",
+	// 	MsgIncr:       "1",
+	// 	Data:          d,
+	// }
+	//
+	// var b bytes.Buffer
+	// enc := gob.NewEncoder(&b)
+	// enc.Encode(m)
+	//
+	// err = c.WriteMessage(websocket.BinaryMessage, b.Bytes())
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+	// for {
+	// 	_, message, err := c.ReadMessage()
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 		break
+	// 	}
+	// 	log.Printf("收到消息: %s", message)
+	// }
 	<-done
 }
