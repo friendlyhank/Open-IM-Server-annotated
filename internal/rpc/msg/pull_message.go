fb774c88bf3a3984d4249a699ab1294baa4e1c19
hank <hanguang8888@outlook.com>
2023-07-02 18:25:37 +0800

    max req

--- internal/rpc/msg/pull_message.go
+++ internal/rpc/msg/pull_message.go
@@ -4,18 +4,36 @@ import (
 	commonDB "Open_IM/pkg/common/db"
 	"Open_IM/pkg/common/log"
 	open_im_sdk "Open_IM/pkg/proto/sdk_ws"
+	"Open_IM/pkg/utils"
 	"context"
+	go_redis "github.com/go-redis/redis/v8"
 )
 
+// GetMaxAndMinSeq - 获取最大和最小的seq
 func (rpc *rpcChat) GetMaxAndMinSeq(_ context.Context, in *open_im_sdk.GetMaxAndMinSeqReq) (*open_im_sdk.GetMaxAndMinSeqResp, error) {
 	log.NewInfo(in.OperationID, "rpc getMaxAndMinSeq is arriving", in.String())
 	resp := new(open_im_sdk.GetMaxAndMinSeqResp)
-	m := make(map[string]*open_im_sdk.MaxAndMinSeq)
 	// 获取最大的seq和最小的seq
 	var maxSeq, minSeq uint64
 	var err1, err2 error
 	maxSeq, err1 = commonDB.DB.GetUserMaxSeq(in.UserID)
 	minSeq, err2 = commonDB.DB.GetUserMinSeq(in.UserID)
+	if (err1 != nil && err1 != go_redis.Nil) || (err2 != nil && err2 != go_redis.Nil) {
+		log.NewError(in.OperationID, "getMaxSeq from redis error", in.String())
+		if err1 != nil {
+			log.NewError(in.OperationID, utils.GetSelfFuncName(), err1.Error())
+		}
+		if err2 != nil {
+			log.NewError(in.OperationID, utils.GetSelfFuncName(), err2.Error())
+		}
+		resp.ErrCode = 200
+		resp.ErrMsg = "redis get err"
+		return resp, nil
+	}
+	resp.MaxSeq = uint32(maxSeq)
+	resp.MinSeq = uint32(minSeq)
+	// todo hank
+	return resp, nil
 }
 
 func (rpc *rpcChat) PullMessageBySeqList(_ context.Context, in *open_im_sdk.PullMessageBySeqListReq) (*open_im_sdk.PullMessageBySeqListResp, error) {
