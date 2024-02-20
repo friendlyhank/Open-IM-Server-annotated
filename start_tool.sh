726d574949a9cde401de787e7e021de6363b333f
hank <hanguang8888@outlook.com>
2024-02-20 22:58:05 +0800

    构建服务

--- start_tool.sh
+++ start_tool.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
 
+# 需要替换为自己指定软件路径
 cd ~/software/
 
 # 启动redis服务
 nohup ./redis/bin/redis-server ./redis/bin/redis.conf &
 # 启动zookeeper
 nohup ./kafka/bin/zookeeper-server-start.sh ./kafka/config/zookeeper.properties &
+
+## 启动服务
+
+# 替换成自己的路径
+cd ~/go/src/github.com/OpenIMSDK/Open-IM-Server/cmd/
+
+# 先杀死进程
+killall openimapi
+
+# 构建服务
+go build -o openimapi openim-api/main.go
+
+#后台构建运行服务
+nohup ./openimapi &
+
+
